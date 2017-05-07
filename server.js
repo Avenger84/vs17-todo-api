14d6002708d2dccb4296ea4f1dec651d4f379e9a
Umut Budak <umut.budak@sahibinden.com>
2017-05-07 12:40:21 +0300

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -41,13 +41,16 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, { id: todoId });
-
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+    
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            res.json(todo.toJSON());
+        } else {
+            res.status(404).json({ "error": "no todo found with that id" });
+        }
+    }, function (e) {
+        res.status(400).json(e);
+    });
 });
 
 // POST /todos
